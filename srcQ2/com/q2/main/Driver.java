cfafdee2f20e193fb99bb759789d75a31a26ff5e
Bhavya Madan <bhavya.madan@outlook.com>
2022-02-06 22:47:46 +0100

    added missing file in q2

--- srcQ2/com/q2/main/Driver.java
+++ srcQ2/com/q2/main/Driver.java
@@ -1,9 +1,42 @@
 package com.q2.main;
 
-public class Driver {
+import com.q2.service.BSTtoRightSkewed;
 
+public class Driver {
+	
+	public static class Node {
+		public Node left;
+	    public Node right;
+	    public int data;
+	    
+	};
+	
+	//Create a new Node
+	public static Node newNode(int nodeData)
+	{
+	    Node temp = new Node();
+	 
+	    temp.data = nodeData;
+	    temp.left = null;
+	    temp.right = null;
+	 
+	    return temp;
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		BSTtoRightSkewed rst = new BSTtoRightSkewed();
+		
+		Node tree = newNode(50);
+		tree.left = newNode(30);
+		tree.right = newNode(60);
+		tree.left.left = newNode(10);
+		//tree.left.right = newNode(20);
+		tree.right.left = newNode(55);
+		//tree.right.left.left =newNode(53);
+		
+		int order = 0;
+		Node headNode =rst.flattenBTToSkewed(tree, order);
+		rst.traverseRightSkewed(headNode);
 
 	}
 
